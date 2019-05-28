0db38997993f605f9b1692b079903c4374899f62
jbays <jjbaize@gmail.com>
2019-05-27 22:49:24 -0500

    Fixed tests for the first function.  Now called genSchedObject.

--- tests/orthly-2-test.js
+++ tests/orthly-2-test.js
@@ -1,42 +1,51 @@
 const expect = require('chai').expect;
-const { mockSchedules, genTimeSlots } = require('../orthly-2');
+const { mockSchedules, mockOperatories, genSchedObject } = require('../orthly-2');
 
 describe('all functions to complete this coding assessment: \n',()=>{
-  describe('genTimeSlots',()=>{
-    let allPossibleTimeSlots = genTimeSlots(mockSchedules);
+  describe('genSchedObject',()=>{
+    let allPossibleTimeSlots = genSchedObject(mockSchedules,mockOperatories);
+    let availability = allPossibleTimeSlots["31/12/2017"]
 
     it('should be a function',()=>{
-      expect(genTimeSlots).to.be.a('function');
+      expect(genSchedObject).to.be.a('function');
     });
-    it('should generate timeSlots for every dentist in schedules',()=>{
-      let allDentists = Object.keys(allPossibleTimeSlots);
-      
-      expect(allDentists[0]).to.equal('1');
-      expect(allDentists[1]).to.equal('2');
-      expect(allDentists.length).to.equal(2);
-    });
+    it('should list 2 dentists with availability on 31 december 2017',()=>{
+      let numberOfDentistsListed = Object.keys(availability).length;
+      expect(numberOfDentistsListed).to.equal(2);
+    })
     it('should generate correct amount of timeSlots (given dentist availability)',()=>{
-      let dentistOnesTimeSlots = allPossibleTimeSlots[1];
-      let dentistTwosTimeSlots = allPossibleTimeSlots[2];
+      let dentistOnesTimeSlots = allPossibleTimeSlots["31/12/2017"]["1"]["1"];
+      let dentistTwosTimeSlots = allPossibleTimeSlots["31/12/2017"]["2"]["2"];
       
       expect(dentistOnesTimeSlots.length).to.equal(8);
       expect(dentistTwosTimeSlots.length).to.equal(8);
     });
     describe('should output an object whose',()=>{
       it('outermost keys are provider numbers',()=>{
-        let hasDentistOne = allPossibleTimeSlots.hasOwnProperty('1');
-        let hasDentistTwo = allPossibleTimeSlots.hasOwnProperty('2');
+        let hasDentistOne = availability.hasOwnProperty('1');
+        let hasDentistTwo = availability.hasOwnProperty('2');
   
         expect(hasDentistOne).to.equal(true);
         expect(hasDentistTwo).to.equal(true);
       });
       it('values are arrays',()=>{
-        let hasDentistOne = Array.isArray(allPossibleTimeSlots['1']);
-        let hasDentistTwo = Array.isArray(allPossibleTimeSlots['2']);
+        let hasDentistOne = Array.isArray(availability['1']['1']);
+        let hasDentistTwo = Array.isArray(availability['2']['2']);
   
         expect(hasDentistOne).to.equal(true);
         expect(hasDentistTwo).to.equal(true);
       });
     })
   });
+  xdescribe('genSchedObjectSkeleton',()=>{
+    let providerNumbersFiledByDate
+    describe('should output an object whose',()=>{
+      it('root keys are date-string objects in DD/MM/YYYY format',()=>{
+        
+      })
+      it('children keys are provider numbers',()=>{
+
+      })
+    })
+  })
 });
